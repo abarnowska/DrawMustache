52547dc3c0396da731e3865df4ed20dfe2078a9d
Angelika Barnowska-Nawara <angelika.barnowska@gmail.com>
2023-08-28 22:08:53 +0200

    adding mobile functionality

--- index.js
+++ index.js
@@ -124,6 +124,25 @@ canvas.addEventListener("mouseup", e => {
 
 canvas.addEventListener("mousemove", drawingFn);
 
+// mobile
+
+canvas.addEventListener("touchstart", function (e) {
+        mousePos = getTouchPos(canvas, e);
+  const touch = e.touches[0];
+  const mouseEvent = new MouseEvent("mousedown", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+
+
+canvas.addEventListener("touchend", function (e) {
+  const mouseEvent = new MouseEvent("mouseup", {});
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+
+
 canvas.addEventListener("touchmove", function (e) {
   const touch = e.touches[0];
   const mouseEvent = new MouseEvent("mousemove", {
@@ -132,5 +151,40 @@ canvas.addEventListener("touchmove", function (e) {
   });
   canvas.dispatchEvent(mouseEvent);
 }, false);
+
+function getTouchPos(canvasDom, touchEvent) {
+  const rect = canvasDom.getBoundingClientRect();
+  return {
+    x: touchEvent.touches[0].clientX - rect.left,
+    y: touchEvent.touches[0].clientY - rect.top
+  };
+}
+
+document.body.addEventListener("touchstart", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+
+
+document.body.addEventListener("touchend", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+
+
+document.body.addEventListener("touchmove", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+
+
+
+
+
+
+
 
 
